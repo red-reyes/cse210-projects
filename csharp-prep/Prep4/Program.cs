6f43172fc31d38f835e290aa5f79dd5d2770e318
helloredreyes <hello.silentlymine@gmail.com>
2024-09-22 10:08:53 +0700

    Added lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when done.");
+
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number !=0)
+            {
+                numbers.Add(number);
+            }
+        } while (number !=0);
+
+        //sum up
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        //calculate the average
+        double average = (double)sum / numbers.Count;
+
+        //find maximum
+        int maxNumber = int.MinValue;
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+
+        //find smallest positive number
+        int smallestPositive = int.MaxValue;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < smallestPositive)
+            {
+                smallestPositive = num;
+            }
+        }
+
+        //sort the list
+        numbers.Sort();
+
+        //display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
+        //the smallest positive number if found
+        if (smallestPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else{
+            Console.WriteLine("There are no positive numbers.");
+        }
+
+        //display sorted list
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
