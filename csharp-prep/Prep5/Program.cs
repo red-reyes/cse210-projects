3fe283f22fd0d9a79567f65a816bf878cbe3f794
helloredreyes <hello.silentlymine@gmail.com>
2024-09-22 10:37:19 +0700

    Added functionality

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // call the functions
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = squaredNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    //function to display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    //function to prompt and return the user's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    //function to prompt and return user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    //square the number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    
+    //display result
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
