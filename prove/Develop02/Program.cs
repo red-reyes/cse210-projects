4409547ecff5433c8b093607d709e98047510ea8
helloredreyes <hello.silentlymine@gmail.com>
2024-09-27 10:57:20 +0700

    transferred class stubs to Dev 2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,10 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[]args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new.Journal();
+        Console.WriteLine(Journal Program);
     }
 }
