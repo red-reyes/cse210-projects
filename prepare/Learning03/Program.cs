17f34ff249e79f2e57f3b59d52571da8ef9947e1
helloredreyes <hello.silentlymine@gmail.com>
2024-10-03 00:46:06 +0700

    Changed decimal values to digits

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -13,24 +13,25 @@ class Program
         //display fractional and decimal values
         Console.WriteLine("Fraction 1");
         Console.WriteLine(fraction1.GetFractionString());
-        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction1.GetDecimalValue().ToString("F2"));
 
-        Console.WriteLine("Fraction 2");
+        Console.WriteLine("\nFraction 2");
         Console.WriteLine(fraction2.GetFractionString());
-        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction2.GetDecimalValue().ToString("F2"));
 
-        Console.WriteLine("Fraction 3");
+        Console.WriteLine("\nFraction 3");
         Console.WriteLine(fraction3.GetFractionString());
-        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction3.GetDecimalValue().ToString("F2"));
 
-        Console.WriteLine("Fraction 4");
+        Console.WriteLine("\nFraction 4");
         Console.WriteLine(fraction4.GetFractionString());
-        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction4.GetDecimalValue().ToString("F2"));
 
         //test getters and setters
         fraction1.SetTop(6);
         fraction1.SetBottom(17);
+        Console.WriteLine("\nTesting Getters and Setters:");
         Console.WriteLine(fraction1.GetFractionString());
-        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction1.GetDecimalValue().ToString("F2"));
     }
 }
