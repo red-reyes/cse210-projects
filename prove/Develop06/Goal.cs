3aa27b7e75466d3d1f18d40b97b354aa1c509a6f
helloredreyes <hello.silentlymine@gmail.com>
2024-10-29 00:31:20 +0700

    edit notes

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -29,17 +29,4 @@ public abstract class Goal
     {
         return _points;
     }
-    /*
-    For example
-
-    Current total points: 50 (current total points from completed tasks accumulates as list updates)
-    [✔] 1. Wash Clothes.
-    [ ] 2. Go to temple. [1/3] //if user sets 30 points, it points to get for each count is target points/taskCount such as 30/3 = 10 points each count until [3/3], user gets bonus of 500 when [3/3] or the targetCount is complete
-    [ ] 3. Drink water. [3/8]
-    [ ] 4. Read the sciptures.
-
-    GetDetailsString - This method should return the details of a goal that could be shown in a list. It should include the checkbox, the short name, and description. Then in the case of the ChecklistGoal class, it should be overridden to shown the number of times the goal has been accomplished so far.
-    
-    GetStringRepresentation - This method should provide all of the details of a goal in a way that is easy to save to a file, and then load later.
-    */
 }
